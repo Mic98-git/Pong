d31f383abadac341f182d97d054f42b455df00bc
ValerioVa96 <valeriovalente96@gmail.com>
2022-01-28 10:56:16 +0100

    edit profile accomplished

--- PongGame/app/src/main/java/com/example/ponggame/fragments/UserProfileFragment.kt
+++ PongGame/app/src/main/java/com/example/ponggame/fragments/UserProfileFragment.kt
@@ -1,12 +1,20 @@
 package com.example.ponggame.fragments
 
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
@@ -17,6 +25,7 @@ import com.example.ponggame.databinding.FragmentUserProfileBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import de.hdodenhof.circleimageview.CircleImageView
 import java.io.File
 
 class UserProfileFragment : Fragment() {
@@ -28,6 +37,20 @@ class UserProfileFragment : Fragment() {
     private lateinit var usernameInputText: EditText
     private lateinit var userUsername: TextView
     private var editing: Boolean = false
+    private lateinit var addImageButton: ImageButton
+    private lateinit var logOutButton: Button
+    private lateinit var undoButton: Button
+    private lateinit var confirmButton: Button
+    private lateinit var editUsernameButton: ImageButton
+
+    private lateinit var builder: AlertDialog.Builder
+    private lateinit var alert: AlertDialog
+    private lateinit var toast: Toast
+
+    // Variables for image update
+    private var imageSelected: Boolean = false
+    private lateinit var profileImage: CircleImageView
+    private lateinit var profileUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,44 +68,92 @@ class UserProfileFragment : Fragment() {
         setProfilePicture()
 
         if (DatabaseImpl.getCurrentUserId().isNotEmpty()) {
-            DatabaseImpl.getUsersReference().child(DatabaseImpl.getCurrentUserId()).addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    view.findViewById<TextView>(R.id.user_email).text = snapshot.child("email").value.toString()
-                    view.findViewById<TextView>(R.id.user_username).text = snapshot.child("username").value.toString()
-                    view.findViewById<TextView>(R.id.user_score).text = snapshot.child("score").value.toString()
-                }
-                override fun onCancelled(error: DatabaseError) {
-                    Toast.makeText(
-                        context,
-                        "Error retrieving your data",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            })
+            DatabaseImpl.getUsersReference().child(DatabaseImpl.getCurrentUserId())
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        view.findViewById<TextView>(R.id.user_email).text =
+                            snapshot.child("email").value.toString()
+                        view.findViewById<TextView>(R.id.user_username).text =
+                            snapshot.child("username").value.toString()
+                        view.findViewById<TextView>(R.id.user_score).text =
+                            snapshot.child("score").value.toString()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        toast = Toast.makeText(
+                            context,
+                            "Error retrieving your data",
+                            Toast.LENGTH_SHORT
+                        )
+                        toast.show()
+                    }
+                })
         }
 
+        // Initialize builder for alert
+        builder = AlertDialog.Builder(this.context!!)
+        builder.setTitle("Attention")
+        builder.setMessage("Username field must be not empty")
+        builder.setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, which ->
+            dialog.dismiss()
+        })
+
         // Initialize buttons
-        val logOutButton = view.findViewById<Button>(R.id.log_out_button)
-        val undoButton = view.findViewById<Button>(R.id.undo_update_button)
-        val confirmButton = view.findViewById<Button>(R.id.confirm_update_button)
+        addImageButton = view.findViewById(R.id.add_profile_image_button)
+        logOutButton = view.findViewById(R.id.log_out_button)
+        undoButton = view.findViewById(R.id.undo_update_button)
+        confirmButton = view.findViewById(R.id.confirm_update_button)
+        editUsernameButton = view.findViewById(R.id.edit_username_button)
 
 
-        // Handling edit text buttons
+        // Handling edit text and image
         userUsername = view.findViewById(R.id.user_username)
         usernameInputText = view.findViewById(R.id.user_username_input)
-        val editUsernameButton = view.findViewById<ImageButton>(R.id.edit_username_button)
+        profileImage = view.findViewById(R.id.profile_image)
+
+
         editUsernameButton.setOnClickListener {
-            if(!editing) {
+            if (!editing) {
                 editing = true
-                editUsernameButton.visibility = View.INVISIBLE
-                userUsername.visibility = View.INVISIBLE
-                usernameInputText.visibility = View.VISIBLE
-                logOutButton.visibility = View.INVISIBLE
-                undoButton.visibility = View.VISIBLE
-                confirmButton.visibility = View.VISIBLE
+                editInputTextVisibilityOn()
+                editButtonsVisibilityOn()
             }
         }
 
+        confirmButton.setOnClickListener {
+            if (editing) {
+                if (!usernameInputText.text.toString().equals("")) {
+                    DatabaseImpl.updateUserUsername(usernameInputText.text.toString())
+                    editInputVisibilityOff()
+                    editButtonsVisibilityOff()
+                    toast = Toast.makeText(
+                        this.context,
+                        "Username successfully updated!",
+                        Toast.LENGTH_SHORT
+                    )
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+                    toast.show()
+                    editing = false
+                } else {
+                    alert = builder.create()
+                    alert.show()
+                }
+            }
+        }
+
+        undoButton.setOnClickListener {
+            editing = false
+            editInputVisibilityOff()
+            editButtonsVisibilityOff()
+        }
+
+        addImageButton.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            //intent.action = Intent.ACTION_GET_CONTENT
+            intent.action = Intent.ACTION_PICK //image from gallery
+            selectImageFromGallery.launch(intent)
+        }
 
         logOutButton.setOnClickListener {
             logOut()
@@ -93,6 +164,9 @@ class UserProfileFragment : Fragment() {
     }
 
     override fun onDestroyView() {
+        if (editing) {
+            undoButton.performClick()
+        }
         super.onDestroyView()
         _binding = null
     }
@@ -104,17 +178,56 @@ class UserProfileFragment : Fragment() {
     private fun logOut() {
         DatabaseImpl.logOut()
         Toast.makeText(
-                context,
-                "You are successfully logged out",
-                Toast.LENGTH_SHORT
-            ).show()
+            context,
+            "You are successfully logged out",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     private fun setProfilePicture() {
         val localFile = File.createTempFile("tempImage", "")
         DatabaseImpl.getProfilePicture(localFile).addOnSuccessListener {
             val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-            binding.profileImage.setImageBitmap(bitmap)
+            profileImage.setImageBitmap(bitmap)
         }
     }
+
+    private fun editInputTextVisibilityOn() {
+        userUsername.visibility = View.INVISIBLE
+        usernameInputText.visibility = View.VISIBLE
+    }
+
+    private fun editInputVisibilityOff() {
+        userUsername.visibility = View.VISIBLE
+        usernameInputText.visibility = View.INVISIBLE
+    }
+
+    private fun editButtonsVisibilityOn() {
+        editUsernameButton.visibility = View.INVISIBLE
+        logOutButton.visibility = View.INVISIBLE
+        undoButton.visibility = View.VISIBLE
+        confirmButton.visibility = View.VISIBLE
+    }
+
+    private fun editButtonsVisibilityOff() {
+        editUsernameButton.visibility = View.VISIBLE
+        logOutButton.visibility = View.VISIBLE
+        undoButton.visibility = View.INVISIBLE
+        confirmButton.visibility = View.INVISIBLE
+    }
+
+    private val selectImageFromGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val res: Intent? = result.data
+                if (res != null) run {
+                    profileUri = res.data!!
+                    profileImage = binding.root.findViewById(R.id.profile_image)
+                    profileImage.setImageURI(profileUri)
+                    imageSelected = true
+                    DatabaseImpl.updateProfilePicture(profileUri)
+                }
+            }
+        }
+
 }
